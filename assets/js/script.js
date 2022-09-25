94377515fe96ed72eab40de25b926e8040cca537
Gu Bicheng <e0374027@u.nus.edu>
2022-09-26 03:07:26 +0800

    Added search bar with react Added result filtering based on search query

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: ''
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(e) {
+    const {
+      value
+    } = e.target;
+    this.setState({
+      inputValue: value
+    });
+  }
+
+  render() {
+    const {
+      inputValue
+    } = this.state;
+    return /*#__PURE__*/React.createElement("div", {
+      className: "input-wrapper"
+    }, /*#__PURE__*/React.createElement("input", {
+      onChange: this.onInputChange,
+      placeholder: " Search by skill tags (eg. sql or r)",
+      value: inputValue,
+      id: "search-input",
+      spellCheck: false
+    }), /*#__PURE__*/React.createElement("span", {
+      className: "input-highlight"
+    }, inputValue.replace(/ /g, "\u00a0")));
+  }
+
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('root'));
+
+
---
+++ assets/js/search_bar.js
@@ -0,0 +1,18 @@
+document.querySelector('#search-input').addEventListener('input',filterElement)
+
+function filterElement(){
+	const searchInput = document.querySelector ('#search-input')
+	const filter = searchInput.value.toLowerCase()
+	const articleElements = document.querySelectorAll('.work-item');
+
+	articleElements.forEach((element) =>{
+		let text = element.textContent;
+		console.log(text)
+		if(text.toLowerCase().includes('#'.concat(filter.toLowerCase()))){
+			element.style.display = '';
+		} else {
+			element.style.display ='none';
+		}
+	});
+
+}
